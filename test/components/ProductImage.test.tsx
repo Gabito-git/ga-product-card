fafdb4391a2aa6fdb2f28170e714443c3136526a
LUIS AREVALO <luisgabrielarevalo@hotmail.com>
2022-01-09 09:58:46 -0500

    Version 0.0.1 lista

---
+++ test/components/ProductImage.test.tsx
@@ -0,0 +1,28 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+import { ProductImage, ProductCard } from '../../src/components';
+import { product2 } from '../data/products';
+
+describe('Product Image', () => {
+    test('Debe mostrar el componente correctamente con la imagen personalizada', () => {
+        const wrapper = renderer.create(
+            <ProductImage img='./hola.png'/>
+        )
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    })
+
+    test('Debe mostrar el componente con la imagen del producto', () => {
+        const wrapper = renderer.create(
+            <ProductCard product={ product2 }>
+                {
+                    () => (
+                        <ProductImage />
+                    )
+                }
+            </ProductCard>
+        )
+
+        expect(wrapper.toJSON()).toMatchSnapshot();
+    })
+})
